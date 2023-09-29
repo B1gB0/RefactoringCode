1dbfed7bfff9b680ae9cc1738ac601c35d034578
B1gB0 <grand0077@inbox.ru>
2023-09-29 17:55:51 +0300

    Fix 1

--- PathFinding.cs
+++ PathFinding.cs
@@ -2,25 +2,25 @@ using UnityEngine;
 
 public class PathFinding : MonoBehaviour
 {
-    private Transform _allTargetPoints;
+    private Transform _target;
     private Transform[] _targetPoints;
     private float _speed;
     private int _numberPoint;
 
     private void Start()
     {
-        _targetPoints = new Transform[_allTargetPoints.childCount];
+        _targetPoints = new Transform[_target.childCount];
 
-        for (int i = 0; i < _allTargetPoints.childCount; i++)
-            _targetPoints[i] = _allTargetPoints.GetChild(i).GetComponent<Transform>();
+        for (int i = 0; i < _target.childCount; i++)
+            _targetPoints[i] = _target.GetChild(i).GetComponent<Transform>();
     }
 
     private void Update()
     {
-        Transform _targetPoint = _targetPoints[_numberPoint];
-        transform.position = Vector3.MoveTowards(transform.position, _targetPoint.position, _speed * Time.deltaTime);
+        Transform targetPoint = _targetPoints[_numberPoint];
+        transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, _speed * Time.deltaTime);
 
-        if (transform.position == _targetPoint.position)
+        if (transform.position == targetPoint.position)
             SetDirection();
     }
 
